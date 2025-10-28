23f73cf3a2e32553e22736a3f5e8683da39d05ad
weslife911 <mfonfubikoma@gmail.com>
2025-10-28 07:35:11 +0100

    added cron to the server and teset api route

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,10 @@
-import express, { Application } from "express"
+import express, { Application, Request, Response } from "express"
 import { config } from "dotenv"
 import { connectToDb } from "./db/connectToDb";
 import AuthRoutes from "./routes/AuthRoutes"
 import SCCRoutes from "./routes/SCCRoutes"
 import cors from "cors"
+import job from "./lib/cron"
 
 config();
 
@@ -22,10 +23,19 @@ app.use(cors({
 app.use("/api/v1", AuthRoutes);
 app.use("/api/v1", SCCRoutes);
 
+app.get("/health", (req: Request, res: Response) => {
+    return res.status(200).json({
+        status: 'healthy',
+        uptime: process.uptime(),
+        timestamp: Date.now()
+    });
+})
+
 const PORT: number = parseInt(process.env.PORT || '8080', 10) || 8080;
 
 app.listen(PORT, "0.0.0.0", () => {
     connectToDb();
     console.log(`Server is running on port ${PORT}`);
+    job.start();
 })
 
